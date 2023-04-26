2ceb048735cfbdec6e412439ea4b623a1adf9efa
bbt420 <123214690+bbt420@users.noreply.github.com>
2023-04-26 13:19:42 +0100

    Update exits.c

--- exits.c
+++ exits.c
@@ -4,8 +4,8 @@
  **_strncpy - copies a string
  *@dest: the destination string to be copied to
  *@src: the source string
- *@n: the amount of characters to be copied into the program.
- *Return: the concatenated string as success.
+ *@n: the amount of characters to be copied into the program
+ *Return: the concatenated string as success
  */
 char *_strncpy(char *dest, char *src, int n)
 {
